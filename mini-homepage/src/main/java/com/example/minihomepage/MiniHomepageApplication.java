e14502f62599260c24b6985ca3e4e319f2fa451e
oosuhyun <tngusimg@naver.com>
2023-08-01 16:53:02 +0900

    Initial commit

---
+++ mini-homepage/src/main/java/com/example/minihomepage/MiniHomepageApplication.java
@@ -0,0 +1,13 @@
+package com.example.minihomepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniHomepageApplication.class, args);
+	}
+
+}
---
+++ mini-homepage/src/test/java/com/example/minihomepage/MiniHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minihomepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
